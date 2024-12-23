3feb2807acff27d81b62774ebed66166dc0d8e80
PolkovnikLetov <ilya2018agafonov@mail.ru>
2024-12-23 16:36:51 +0500

    Change build.sh

--- build.sh
+++ build.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# Выходим из скрипта, если какая-либо команда завершится с ошибкой
+
 set -e
 
-# Проверяем, что docker-compose установлен
+
 if ! command -v docker-compose &> /dev/null
 then
     echo "docker-compose не установлен. Пожалуйста, установите его перед запуском скрипта."
     exit 1
 fi
 
-# Собираем и запускаем проект с помощью docker-compose
+
 echo "Запускаем Docker Compose..."
 docker-compose up -d --build
 
-echo "Проект запущен. Вы можете открыть браузер и перейти по адресу http://localhost:5000"
+echo "http://localhost:5000"
